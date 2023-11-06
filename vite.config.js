b0b1bfa90d8f30db57296289f32ebdc277f54149
pliavi <pliavi.rpgm@gmail.com>
2023-11-05 23:19:41 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+import { createHtmlPlugin } from "vite-plugin-html";
+
 export default {
   base: "./",
-  // plugins: [
-  //   createHtmlPlugin({
-  //     minify: true,
-  //     entry: "src/js/index.js",
-  //   }),
-  // ],
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+      entry: "src/js/index.js",
+    }),
+  ],
 };
